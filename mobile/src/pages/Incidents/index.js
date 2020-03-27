539e97490f0f6c3932f43e57badf069bca315a1d
andregm <andre@appdev.com.br>
2020-03-27 04:59:26 -0300

    Hotfix:  useState page default 1

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -9,7 +9,7 @@ import styles from './styles';
 export default function Incidents(){
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
